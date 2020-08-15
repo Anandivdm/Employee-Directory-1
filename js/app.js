4b91f1faca6205260ae8b03d24d88573d1a4f911
Anandi Ellis <anandivdm@gmail.com>
2020-08-15 10:43:43 +0200

    Modal

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
+let employees = [];
 let employeeCards = document.getElementById('employee-cards');
+let overlay = document.getElementById('overlay');
+let closeModal = document.getElementById('close');
+let modalDetails = document.getElementById('modal-details');
 
 // Fetch 12 users
 
@@ -20,13 +24,51 @@ function generateEmployee(data) {
     let email = employee.email
     let location = employee.location.city
     employeeHTML += `
-      <div class="card">
+      <div class="card" data-index="${index}">
         <div class="card-details">
           <img src="${image}" alt>
-          <h2>${firstName} ${lastName}</h2>
-          <p>${email}</p>
+          <div class="employee-details">
+            <h2>${firstName} ${lastName}</h2>
+            <p>${email}</p>
+            <p>${location}</p>
+          </div>
         </div>
       </div>`
   });
   employeeCards.innerHTML = employeeHTML;
 };
+
+function showModal(index) {
+  console.log(index)
+  console.log(employees[index])
+  let modalHTML;
+
+  modalHTML = `
+  <p>Hello</p>
+  `
+  modalDetails.innerHTML = modalHTML;
+}
+
+// Event Listener
+
+employeeCards.addEventListener(('click'), e => {
+  let card = e.target.closest(".card");
+  let index = card.getAttribute('data-index');
+  overlay.style.display = 'unset';
+  showModal(index)
+})
+
+closeModal.addEventListener(('click'), e => {
+  overlay.style.display = 'none';
+})
+
+{/* <div class="employee-details">
+      <h2>${firstName} ${lastName}</h2>
+      <p>hello</p>
+      <p>${location}</p>
+      <hr>
+        <p>${phone}</p>
+        <p>${location}</p>
+        <p>Birthday: ${dob}</p>
+    </div> */}
+
